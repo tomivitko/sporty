5b564fb3e0592b43afe035f3474519afc50726ba
tomivitko <tomi.vitko@gmail.com>
2017-01-19 22:01:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_start();
 ?>
 <html>
 	<head>
-		<title>Introspect by TEMPLATED</title>
+		<title>sporty</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
